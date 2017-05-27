82984f95adb894c3a68487045f4556a52b9a94d3
Mujtaba Asif <mjtbasif@gmail.com>
2017-05-27 14:31:40 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace ClubExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textname = new System.Windows.Forms.TextBox();
+            this.textid = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textphone = new System.Windows.Forms.TextBox();
+            this.textmail = new System.Windows.Forms.TextBox();
+            this.texttype = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(412, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit Response";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textname
+            // 
+            this.textname.Location = new System.Drawing.Point(176, 43);
+            this.textname.Multiline = true;
+            this.textname.Name = "textname";
+            this.textname.Size = new System.Drawing.Size(248, 36);
+            this.textname.TabIndex = 1;
+            // 
+            // textid
+            // 
+            this.textid.Location = new System.Drawing.Point(176, 94);
+            this.textid.Multiline = true;
+            this.textid.Name = "textid";
+            this.textid.Size = new System.Drawing.Size(248, 36);
+            this.textid.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 36);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 36);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Phone";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 36);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "E-mail";
+            // 
+            // textphone
+            // 
+            this.textphone.Location = new System.Drawing.Point(176, 144);
+            this.textphone.Multiline = true;
+            this.textphone.Name = "textphone";
+            this.textphone.Size = new System.Drawing.Size(248, 36);
+            this.textphone.TabIndex = 7;
+            // 
+            // textmail
+            // 
+            this.textmail.Location = new System.Drawing.Point(176, 197);
+            this.textmail.Multiline = true;
+            this.textmail.Name = "textmail";
+            this.textmail.Size = new System.Drawing.Size(248, 36);
+            this.textmail.TabIndex = 8;
+            // 
+            // texttype
+            // 
+            this.texttype.Location = new System.Drawing.Point(176, 251);
+            this.texttype.Multiline = true;
+            this.texttype.Name = "texttype";
+            this.texttype.Size = new System.Drawing.Size(248, 36);
+            this.texttype.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(27, 254);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 36);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Comment";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Calibri Light", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(96, 394);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(266, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Created by Md. Mujtaba Asif";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(441, 29);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(65, 25);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(47, 25);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(441, 445);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.texttype);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textmail);
+            this.Controls.Add(this.textphone);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textid);
+            this.Controls.Add(this.textname);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EWU Clubs";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textname;
+        private System.Windows.Forms.TextBox textid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textphone;
+        private System.Windows.Forms.TextBox textmail;
+        private System.Windows.Forms.TextBox texttype;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace ClubExcel
+{
+    public partial class Form1 : Form
+    { 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void openSheet()
+        {
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string extension = ".xlsx";
+            filePath += @"\responses" + extension;
+
+            if(File.Exists(filePath))
+            {
+                Excel.Application xApp = new Excel.Application();
+                Excel.Workbook xBook = xApp.Workbooks.Open(filePath);
+                Excel.Worksheet xSheet = xApp.ActiveSheet as Microsoft.Office.Interop.Excel.Worksheet;
+                xApp.Visible = false;
+
+                int c, add;
+                string name, id, phone,email, type;
+
+                Excel.Range userRange = xSheet.UsedRange;
+                c = userRange.Rows.Count;
+                add = c + 1;
+                name = textname.Text;
+                id = textid.Text;
+                phone = textphone.Text;
+                email = textmail.Text;
+                type = texttype.Text;
+                xSheet.Cells[add, 1] = (name);
+                xSheet.Cells[add, 2] = (id);
+                xSheet.Cells[add, 3] = (phone);
+                xSheet.Cells[add, 4] = (email);
+                xSheet.Cells[add, 5] = (type);
+                textname.Text = "";
+                textid.Text = "";
+                textphone.Text = "";
+                textmail.Text = "";
+                texttype.Text = "";
+
+                xApp.DisplayAlerts = false;
+                xBook.Save();
+                xBook.Close();
+                xApp.Quit();
+            }
+            else
+            {
+                Excel.Application xApp = new Excel.Application();
+                Excel.Workbook xBook = xApp.Workbooks.Add(true);
+                Excel.Worksheet xSheet = xApp.ActiveSheet as Microsoft.Office.Interop.Excel.Worksheet;
+                xApp.Visible = true;
+
+                //initializing the preadsheet
+                xSheet.Cells[1, 1] = ("Name");
+                xSheet.Cells[1, 2] = ("ID");
+                xSheet.Cells[1, 3] = ("Phone");
+                xSheet.Cells[1, 4] = ("E-mail");
+                xSheet.Cells[1, 5] = ("Comment");
+
+                //inporting first data
+                int c, add;
+                string name, id, phone, email, type;
+
+                Excel.Range userRange = xSheet.UsedRange;
+                c = userRange.Rows.Count;
+                add = c + 1;
+                name = textname.Text;
+                id = textid.Text;
+                phone = textphone.Text;
+                email = textmail.Text;
+                type = texttype.Text;
+                xSheet.Cells[add, 1] = (name);
+                xSheet.Cells[add, 2] = (id);
+                xSheet.Cells[add, 3] = (phone);
+                xSheet.Cells[add, 4] = (email);
+                xSheet.Cells[add, 5] = (type);
+                textname.Text = "";
+                textid.Text = "";
+                textphone.Text = "";
+                textmail.Text = "";
+                texttype.Text = "";
+
+                //closing current operation
+                xApp.DisplayAlerts = false;
+                xBook.SaveAs(filePath, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
+            false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+            Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                xApp.Quit();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openSheet();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This application was created by\n\nMd. Mujtaba Asif\nDepartment of Computer Science and Engineering\nEast West University\n", "Thank You", MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
